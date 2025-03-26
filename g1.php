5084224509929e5a2565de8b2f77ae25e2dd38d2
Anaghan Namita <anaghannamita@gmail.com>
2025-03-26 22:25:33 +0530

    g1

---
+++ g1.php
@@ -0,0 +1,20 @@
+<htm>
+    <head>
+        <title> form </title>
+    </head>
+     <body>
+        <table border="2" align="center">
+            <h1> registration </h1>
+            Name:
+             <input tyep="text" name="name"><br><br>
+            Email:
+             <input type="email" name="email" id="email"><br><br>
+            password:
+             <input type="password" name="password"><br><br>
+            <input type="submit" name="submir" value="submit">
+
+        </table>
+     </body>
+</html>
+<?php
+?>
